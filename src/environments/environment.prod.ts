7d6b2e042fd360ed95d8c73257f53726da3eb4bc
jose marte <tumusicalatina@gmail.com>
2019-04-06 10:13:30 -0400

    Updated routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase :{
+    apiKey: "AIzaSyARjQEltQv5M9qYKnqultqk9LXvw9Yw0z0",
+    authDomain: "todolist-5a0b8.firebaseapp.com",
+    databaseURL: "https://todolist-5a0b8.firebaseio.com",
+    projectId: "todolist-5a0b8",
+    storageBucket: "todolist-5a0b8.appspot.com",
+    messagingSenderId: "624640234454"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase :{
+    apiKey: "AIzaSyARjQEltQv5M9qYKnqultqk9LXvw9Yw0z0",
+    authDomain: "todolist-5a0b8.firebaseapp.com",
+    databaseURL: "https://todolist-5a0b8.firebaseio.com",
+    projectId: "todolist-5a0b8",
+    storageBucket: "todolist-5a0b8.appspot.com",
+    messagingSenderId: "624640234454"
+  }
 };
 
 /*
